7d09526bcab1c5d811e2192bd8fe64fe90ef51a0
narasimha134 <102607714+narasimha134@users.noreply.github.com>
2022-03-29 20:43:41 +0530

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,7 +2,7 @@ public class HelloWorld {
 
     public static void main(String[] args) {
         // Prints "Hello, World" to the terminal window.
-        System.out.println("Hello, New World");
+        System.out.println("Hello, New1 World");
     }
 
 }
